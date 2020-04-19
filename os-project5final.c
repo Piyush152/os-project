131f1103cc001026c1c73e74a532c1f5d44d3c63
Piyush152 <43949277+Piyush152@users.noreply.github.com>
2020-04-20 04:30:19 +0530

    Update os-project5final.c

--- os-project5final.c
+++ os-project5final.c
@@ -1,54 +1,96 @@
 #include<stdio.h>
-int main()
+#include<stdlib.h>
+#include<conio.h> 
+int main() 
+{ int count,i,j,m=0,n,y=0,time,remain=0,min,flag=0; 
+  int wait_time=0,turn_a_time=0,a_time[10],b_time[10],p[10],z[10];
+  float k=0,x=0; 
+  printf("Enter number of Process:\t "); 
+  scanf("%d",&n); 
+  printf("\n\tArrival time should be greater than 2 as CPU remains idle for first 3 secs.\n");
+  printf("\n\tBurst time should be less tha 10\n");
+  for(count=0;count<n;count++) 
+  { 
+    printf("Enter Arrival Time and Burst Time for Process Process Number %d :",count+1); 
+	scanf("%d",&a_time[count]); 
+	scanf("%d",&b_time[count]);  
+ } 
+for(i=0;i<n;i++)
 {
-  int count,j,n;
-  int time,remaining;
-  int flag=0,time_quantum=10;
-  int waiting_time=0,turn_around_time=0,arrival_time[10],burst_time[10],rt[10];
-  printf("\n\nEnter the Total number of Process:\t ");
-  scanf("%d",&n);
-  remaining=n;
-  for(count=0;count<n;count++)
-  {
-    printf("Enter Arrival Time and Burst Time for Process Process Number %d :",count+1);
-    scanf("%d",&arrival_time[count]);
-    scanf("%d",&burst_time[count]);
-    rt[count]=burst_time[count];
-  }
-  printf("Enter Time Quantum:%d\t",time_quantum);
- 
-  printf("\n\nProcess\t|Turnaround Time|Waiting Time\n\n");
-  for(time=0,count=0;remaining!=0;)
-  {
-    if(rt[count]<=time_quantum && rt[count]>0)
-    {
-      time+=rt[count];
-      rt[count]=0;
-      flag=1;
-    }
-    else if(rt[count]>0)
-    {
-      rt[count]-=time_quantum;
-      time+=time_quantum;
-    }
-    if(rt[count]==0 && flag==1)
-    {
-      remaining--;
-      printf("P[%d]\t|\t%d\t|\t%d\n",count+1,time-arrival_time[count],time-arrival_time[count]-burst_time[count]);
-      waiting_time+=time-arrival_time[count]-burst_time[count];
-      turn_around_time+=time-arrival_time[count];
-      flag=0;
-    }
-    if(count==n-1)
-      count=0;
-    else if(arrival_time[count+1]<=time)
-      count++;
-    else
-      count=0;
-  }
-  printf("\nAverage Waiting Time= %f\n",waiting_time*1.0/n);
-  printf("Avg Turnaround Time = %f",turn_around_time*1.0/n);
- 
-  return 0;
+	if(a_time[i]==0)
+	{
+		printf("\nS INVALID ARRIVAL TIME\n");
+		getch();
+		exit(1);
+	}
 }
-
+for(i=0;i<n;i++)
+{
+	if(a_time[i]<3)
+	{
+		printf("\nS INVALID Arrival Time it should be greater than 3\n");
+		getch();
+		exit(1);
+	}
+}
+	printf("\n\n\tProcess\t|Turnaround Time|Waiting Time\n\n"); 
+	printf("\t===========================================\n");
+for(i=0;i<n;i++)
+{
+	m=m+b_time[i];
+}
+min=m;
+time=m;
+for(i=0;i<n;i++)
+{
+	if(a_time[i]<time)
+	{
+		time=a_time[i];
+	}
+}
+for(i=time;i<=m;i=i+b_time[j])
+{
+	min=m;
+	remain=0;
+	flag=0;
+	for(count=0;count<n;count++)
+	{
+		if(a_time[count]<=i)
+		{
+			if(b_time[count]<min)
+			{
+				min = b_time[count];
+				j=count;
+				flag=1;
+			}
+			remain=1;	 
+		}
+	}
+	if(flag==1&&remain==1)
+	{
+		wait_time=i-a_time[j];
+		turn_a_time=wait_time+b_time[j];
+		printf("\tP[%d]\t|\t%d\t|\t%d\n",j+1,turn_a_time,wait_time); 
+		k=k+wait_time;
+		x=x+turn_a_time;
+		a_time[j]=m+1;
+		p[y]=j+1;
+		z[y]=i;
+		y++;
+	}
+}
+printf("\n\nAverage Waiting Time= %.2f\n",k/n); 
+printf("Avg Turnaround Time = %.2f",x/n); 
+printf("\n\n\nTotal time taken by processor to complete all the jobs : %d",m);  
+printf("\n\nQueue  for order of execution:\n");
+printf("\n\nProcess		");
+for(i=0;i<n;i++)
+{
+	printf(" P[%d]   ",p[i]);
+	if(i==(n-1))
+	{
+		printf("End");
+	}
+}
+  return 0; 
+}
